d44f43de71ad1529bc0ffa8f905ae903a7290afb
smd <smatondangg91@gmail.com>
2025-04-16 18:12:18 +0700

    Create 1.js

---
+++ 24?7/1.js
@@ -0,0 +1,24 @@
+# ASCII Art
+ascii_art="
+  _____ __  __ _______ _____  
+ / ____|  \/  |__   __|  __ \ 
+| (___ | \  / |  | |  | |  | |
+ \___ \| |\/| |  | |  | |  | |
+ ____) | |  | |  | |  | |__| |
+|_____/|_|  |_|  |_|  |_____/ 
+
+"
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+while true # start an infinite loop
+do
+  echo "SMTD Team, Go 24/7!" # print "SMTD Team, Go 24/7!"
+  sleep 1 # wait for 1 second
+done # end the loop
